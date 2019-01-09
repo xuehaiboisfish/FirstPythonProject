91a06338e9c3b92fcd43aad4ad03ff1cfdb0c442
xuehaiboisfish <xuehaiboisfish@yahoo.co.jp>
2019-01-09 16:20:14 +0800

    pythonProject

---
+++ Test/a.py
@@ -0,0 +1,19 @@
+list = ['a', 1, 'c']
+print(list)
+
+obj = ('aa', 'bb')
+print(obj)
+
+dic = {'a': 1, 'b': 2}
+print(dic.keys())
+print(dic.values())
+
+cnt = 0
+while cnt < 5:
+    print(cnt)
+    cnt += 1
+else:
+    print(cnt)
+
+for num in range(0, 50, 10):
+    print(num)
